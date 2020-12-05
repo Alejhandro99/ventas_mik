a1432889833f1c5aa4ede56d58c847becce97166
Alejhandro99 <alejhandro.alvarado2015@gmail.com>
2020-12-05 03:34:47 -0400

    para desplegar con base de datos

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('ventas_mik', 'root', '', {
-    host: 'localhost',
+const sequelize = new Sequelize('ventas_mik', 'alejhandro99', 'Alejhandro99$', {
+    host: 'db4free.net',
     dialect: 'mysql'
 })
 
